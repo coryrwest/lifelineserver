78df132f84208b14f2b1d898fafac55f644945d0
Cory Westropp <cory.r.west@gmail.com>
2015-08-24 15:04:38 -0700

    Openshift compat

--- server.js
+++ server.js
@@ -83,7 +83,19 @@ module.exports = app;
 
 /* istanbul ignore next */
 if (!module.parent) {
-    var port = process.env.PORT || 80;
-    app.listen(port);
+    var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+    var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+	if (typeof ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            ipaddress = "127.0.0.1";
+        };
+
+    app.listen(port, ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), ipaddress, port);
+	});
     console.log('Express started on port ' + port);
 }
